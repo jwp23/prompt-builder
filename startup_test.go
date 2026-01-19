da0a98ec0b0e575b28b99d6507be666097364094
Joseph Presley <36678465+jwp23@users.noreply.github.com>
2026-01-19 14:41:37 -0800

    test(startup): add quiet mode and non-TTY tests

--- startup_test.go
+++ startup_test.go
@@ -20,3 +20,33 @@ func TestWaitForModel_AlreadyLoaded(t *testing.T) {
 		t.Fatalf("unexpected error: %v", err)
 	}
 }
+
+func TestWaitForModel_QuietMode(t *testing.T) {
+	// Server that would fail if called
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t.Error("server should not be called in quiet mode")
+	}))
+	defer server.Close()
+
+	client := NewOllamaClient(server.URL, "llama3.2")
+	err := WaitForModel(client, true, true) // quiet=true
+
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
+
+func TestWaitForModel_NonTTY(t *testing.T) {
+	// Server that would fail if called
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t.Error("server should not be called in non-TTY mode")
+	}))
+	defer server.Close()
+
+	client := NewOllamaClient(server.URL, "llama3.2")
+	err := WaitForModel(client, false, false) // tty=false
+
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
